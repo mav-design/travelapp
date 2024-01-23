7a40c6575f001f180e1aa188bf5d0b291cb1d34a
mav-design <mav.dev.design@gmail.com>
2024-01-23 13:52:30 +0300

    maintane alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Home } from '@pages/Home/Home';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+const router = createBrowserRouter([{
+  path:"./",
+  element:<Home/>
+  },
+])
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import * as path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve:{
+    alias:
+    [
+      {find:'@app', replacement:path.resolve(__dirname,'./src/app')},
+      {find:'@entities', replacement:path.resolve(__dirname,'./src/entities')},
+      {find:'@page', replacement:path.resolve(__dirname,'./src/pages')},
+      {find:'@widgets', replacement:path.resolve(__dirname,'./src/widgets')},
+      {find:'@shared', replacement:path.resolve(__dirname,'./src/shared')},
+      {find:'@slices', replacement:path.resolve(__dirname,'./src/app/redux/slices')},
+  ],
+    },
+  }
+)
